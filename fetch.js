dbb6499ea16aa6e96b9881a09b870f7be9023f48
周培公 <peigong@foxmail.com>
2014-03-03 18:42:12 +0800

    完成了抓取功能

--- fetch.js
+++ fetch.js
@@ -1,11 +1,15 @@
 
-var nopt = require("nopt")
-  , knownOpts = { "word" : [String, null] }
+var nopt = require("nopt");
+var TopticDirector = require('./lib/toptic-director.js');
+
+var knownOpts = { "word" : [String, null] }
   , shortHands = { "w" : ["--word"] }
   , params = nopt(knownOpts, shortHands, process.argv, 2);
 
 var word = params.word;
 if(word){
+    var director = new TopticDirector();
+    director.fetch(word);
 }else{
     console.log('Usage:node fetch -w word');
 }
