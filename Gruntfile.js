39436b7b339c8c31985112b6a86e2ae5becf6364
周培公 <peigong@foxmail.com>
2014-03-03 22:09:39 +0800

    增加关键词的Bower组件依赖。

---
+++ Gruntfile.js
@@ -0,0 +1,26 @@
+
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+
+    pkg: grunt.file.readJSON('package.json'),
+    copy: {
+      dist: {
+        files: [ 
+          {
+            expand: true,
+            cwd: 'bower_components/weibo-keywords',
+            src: ['keywords.conf'],
+            dest: 'config/'
+          }
+        ]
+      }
+    }
+  });
+  
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  // Default task.
+  grunt.registerTask('default', ['copy']);
+
+};
