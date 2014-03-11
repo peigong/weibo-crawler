aa7a42e17febc6775c0efc53fe0cd6455270d8fc
周培公 <peigong@foxmail.com>
2014-03-11 09:18:37 +0800

    增加twitter时间轴抓取。

--- run.js
+++ run.js
@@ -1,35 +1,43 @@
 
 var nopt = require("nopt");
 var path = require('path');
+var util = require('util');
 
 //var logger = require('./lib/logger.js');
 var TopicDirector = require('./lib/topic-director.js');
 var TweetDirector = require('./lib/tweet-director.js');
 var BaiduDirector = require('./lib/baidu-director.js');
 
-var knownOpts = { "type" : [String, null] }
-  , shortHands = { "t" : ["--type"] }
+var directors = {
+    'sina': TopicDirector,
+    'twitter': TweetDirector,
+    'baidu': BaiduDirector
+};
+
+var knownOpts = { "type" : [String, null], "config" : [String, null] }
+  , shortHands = { "t" : ["--type"], "c" : ["--config"] }
   , params = nopt(knownOpts, shortHands, process.argv, 2);
 
-var type = params.type;
-var filename, director;
+var type = params.type.toLowerCase();
+var config = params.config;
+
+var conf = util.format('%s-keywords.conf', type);
+if (config) {
+    conf = config;
+};
+var conf_file = path.normalize(path.resolve(__dirname, 'config', conf));
+
 switch(type){
     case 'sina':
-        filename = path.normalize(path.resolve(__dirname, 'config', 'sina-keywords.conf'));
-        director = new TopicDirector();
-        director.run(filename);
-        break;
     case 'twitter':
-        filename = path.normalize(path.resolve(__dirname, 'config', 'twitter-keywords.conf'));
-        director = new TweetDirector();
-        director.run(filename);
-        break;
     case 'baidu':
-        filename = path.normalize(path.resolve(__dirname, 'config', 'baidu-keywords.conf'));
-        director = new BaiduDirector();
-        director.run(filename);
+        var director = new directors[type]();
         break;
     default:
-        console.log('Usage:node run -t sina|twitter|baidu');
 }
 
+if (director) {
+    director.run(conf_file);
+}else{
+    console.log('Usage:node run -t sina|twitter|baidu [-c type-keywords.conf]');
+}
